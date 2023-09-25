4f74df09fd1ef9d0a6e94139ede8b3c476efc793
Harika Bandela <124243226+HarikaBandela20@users.noreply.github.com>
2023-09-24 21:13:46 -0700

    Update main.js

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,6 +1,6 @@
 window.onload = () => {
     const footer = document.createElement("footer");
-    const footerContent = document.createTextNode("© Abdul Moqueet | All Rights Reserved "+new Date().getFullYear()+".");
+    const footerContent = document.createTextNode("© Harika Bandela | All Rights Reserved "+new Date().getFullYear()+".");
     footer.appendChild(footerContent);
     document.body.appendChild(footer);
 };
